ff6fbadf5ef9c62c7ceaa2be07181158e8119260
Humayun Ahmed <Humayun.Ahmed@bakerhughes.com>
2022-03-03 19:36:26 +0100

    error handling in frontend

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent implements OnInit{
   title = 'Kras Loterij';
   isScratched=false;
   userId='';
+  message='';
   scratches: ScratchModel[] = [];
   constructor(private appService: AppService) {
     this.userId = this.appService.getUserId();
@@ -23,7 +24,11 @@ export class AppComponent implements OnInit{
   getData(){
     this.appService.getData().subscribe((scratches: any) => {
       this.scratches = scratches;
-    })
+        console.log("Http get called.")
+    },
+      (error) => {                              //Error callback
+        this.errorHandle(error);
+      })
   }
 
   scratch(index: number){
@@ -35,11 +40,28 @@ export class AppComponent implements OnInit{
         if(!this.isScratched)
         {
           this.scratches[index].userId = this.appService.getUserId();
-          this.appService.updateData(this.scratches[index]).subscribe(()=>{})
+          this.appService.updateData(this.scratches[index]).subscribe(()=>{
+            console.log("Http scratch called.")
+            },
+            (error) => {                              //Error callback
+              this.errorHandle(error);
+            })
           this.isScratched = true;
         }
-      })
+      },
+        (error) => {                              //Error callback
+          this.errorHandle(error);
+        })
     }
+    if(this.isScratched)
+    {
+      this.message='Sorry!!! You already scratched.';
+    }
+  }
+  errorHandle(error:any)
+  {
+    console.error(error);
+    this.message='Oops!!! Something is wrong.';
   }
 
   counter(i: number) {
